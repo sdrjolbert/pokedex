ed10775fd3f04c569b4627bdd362d7b6a6d62b1a
Jolbert Sodré <jolbert.sodre15@gmail.com>
2023-06-09 15:31:10 -0300

    Primeiro commit

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Pokedex',
+      title: 'My Pokédex',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
